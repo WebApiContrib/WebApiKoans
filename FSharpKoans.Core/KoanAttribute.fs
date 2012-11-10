5cb88b266f5fe4873bd93ca4c84a1f58f7b80672
Ryan Riley <ryan.riley@panesofglass.org>
2012-11-09 22:21:09 -0600

    Add core koans components.

---
+++ FSharpKoans.Core/KoanAttribute.fs
@@ -0,0 +1,11 @@
+﻿namespace FSharpKoans.Core
+
+open System
+
+[<AttributeUsage(AttributeTargets.Class ||| AttributeTargets.Method, AllowMultiple = false)>]
+type KoanAttribute () =
+    inherit Attribute()
+    let mutable sortOrder = 0
+    member x.Sort
+        with get() = sortOrder
+        and set(v) = sortOrder <- v
---
+++ FSharpKoans.Core/KoanContainer.fs
@@ -0,0 +1,28 @@
+﻿module FSharpKoans.Core.KoanContainer
+
+open System
+open System.IO
+open System.Reflection
+
+let hasKoanAttribute (info:MethodInfo) =
+    info.GetCustomAttributes(typeof<KoanAttribute>, true)
+    |> Seq.isEmpty
+    |> not
+
+let findKoanMethods (container: Type) =
+    container.GetMethods(BindingFlags.Public ||| BindingFlags.Static)
+    |> Seq.filter hasKoanAttribute
+    
+let runKoans container =
+    let getKoanResult (m:MethodInfo) =
+        try
+            m.Invoke(null, [||]) |> ignore
+            Success <| sprintf "%s has expanded your awareness." m.Name
+        with
+        | ex -> 
+            let outcome = sprintf "%s has damaged your karma." m.Name
+            Failure(outcome, ex.InnerException)
+        
+    container
+    |> findKoanMethods
+    |> Seq.map getKoanResult
