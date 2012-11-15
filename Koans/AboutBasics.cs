a08ee8da8e7943791241ad0d83c95260f64e889b
Ryan Riley <ryan.riley@panesofglass.org>
2012-11-14 22:46:12 -0600

    Add another, final assertion to Basics.

--- Koans/AboutBasics.cs
+++ Koans/AboutBasics.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http;
 using FSharpKoans.Core;
@@ -7,7 +8,7 @@ using FSharpKoans.Core;
 namespace Koans
 {
     /**
-     * About Basics
+     * Lesson 1: About Basics
      * 
      * Unlike many koans programs, Web API requires a bit of setup
      * in order to start learning how to use it. Each of the concepts
@@ -23,12 +24,12 @@ namespace Koans
             // Create a configuration. Web API uses a code-based configuration
             // rather than the XML-based configuration familiar to WCF programmers.
             using (var config = new HttpConfiguration())
-            // Create an HttpServer. HttpServer is the core for all other server
-            // implementations. We'll discuss this in more detail later.
+                // Create an HttpServer. HttpServer is the core for all other server
+                // implementations. We'll discuss this in more detail later.
             using (var server = new HttpServer(config))
-            // Create an HttpClient. HttpClient can be used without the server
-            // to hit the public web, but passing in a server will allow you
-            // direct access.
+                // Create an HttpClient. HttpClient can be used without the server
+                // to hit the public web, but passing in a server will allow you
+                // direct access.
             using (var client = new HttpClient(server))
             {
                 // Send a GET message. The Web API API is asynchronous.
@@ -83,20 +84,57 @@ namespace Koans
 
         class OkHandler : DelegatingHandler
         {
-            /**
-             * Message Handlers are very similar to a delegate taking
-             * an HttpRequestMessage and returning a Task returning
-             * an HttpResponseMessage. DelegatingHandlers are an implementation
-             * that allow chaining. Once again, we'll take a look at this
-             * in more detail later.
-             */
-            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+            // Message Handlers are very similar to a delegate taking
+            // an HttpRequestMessage and returning a Task returning
+            // an HttpResponseMessage. DelegatingHandlers are an implementation
+            // that allow chaining. Once again, we'll take a look at this
+            // in more detail later.
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
             {
                 var response = new HttpResponseMessage();
 
                 // The best way to return a synchronous result is by using
                 // a TaskCompletionSource. Look up more information about
                 // Tasks at http://bradwilson.typepad.com/blog/2012/04/tpl-and-servers-pt1.html
+                var tcs = new TaskCompletionSource<HttpResponseMessage>();
+                tcs.SetResult(response);
+                return tcs.Task;
+            }
+        }
+
+        [Koan]
+        public static void HelloWorld()
+        {
+            using (var config = new HttpConfiguration())
+            using (var server = new HttpServer(config))
+            using (var client = new HttpClient(server))
+            {
+                config.Routes.MapHttpRoute("Default", "api");
+                config.MessageHandlers.Add(new HelloHandler());
+
+                var response = client.GetAsync("http://go.com/api").Result;
+                // In addition to getting the response message, you need
+                // to also retrieve the content. Each message object has a
+                // Content property and methods to read the contents.
+                // We will look more into these in AboutContent.
+                var body = response.Content.ReadAsStringAsync();
+
+                Helpers.AssertEquality(Helpers.__, body);
+            }
+        }
+
+        class HelloHandler : DelegatingHandler
+        {
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                var response = new HttpResponseMessage
+                {
+                    // AboutContent will address the various types of HttpContent.
+                    // For now, you may note that the HttpResponseMessage
+                    // has a Content property that will accept such a type.
+                    Content = new StringContent("Hello, world!")
+                };
+
                 var tcs = new TaskCompletionSource<HttpResponseMessage>();
                 tcs.SetResult(response);
                 return tcs.Task;
