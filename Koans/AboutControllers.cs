c7587930844c2c9cf4fb26022b042abf5986540b
Ryan Riley <ryan.riley@panesofglass.org>
2012-11-15 00:06:30 -0600

    Fix AboutControllers.

--- Koans/AboutControllers.cs
+++ Koans/AboutControllers.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -17,30 +16,114 @@ namespace Koans
      * as model binding and formatting.
      */
 
+    [Koan(Sort = 2)]
+    public static partial class AboutControllers
+    {
+        [Koan]
+        public static void SimpleHelloWorldController()
+        {
+            using (var config = new HttpConfiguration())
+            using (var server = new HttpServer(config))
+            using (var client = new HttpClient(server))
+            {
+                // Controllers are identified through the routing mechanism.
+                // Web API includes a convenient extension method to the
+                // HttpRouteCollection type, which is exposed as the
+                // HttpConfiguration.Routes property. We will use the default
+                // uri template and map it using the `MapHttpRoute` extension method.
+                // If you have worked with MVC, this will be very familiar.
+                // In a larger project, this would most likely be defined in
+                // your Global.asax or WebApiConfig.cs file.
+                config.Routes.MapHttpRoute(
+                    name: "Api",
+                    routeTemplate: "api/{controller}"
+                );
+
+                using (var response = client.GetAsync("http://example.org/api/helloworld").Result)
+                {
+                    var body = response.Content.ReadAsStringAsync().Result;
+                    Helpers.AssertEquality("\"Hello, ApiController!\"", body);
+                }
+            }
+        }
+    }
+
     // To use an ApiController, you need to define a subclass.
-    // We will extend and reuse this type throughout this set of koans.
-    class TestController : ApiController
+    // ApiController implements the IHttpController interface
+    // and implements a number of helpful functionality familiar
+    // to MVC developers such as ActionFilters, Model Binding,
+    // content negotiation, and more.
+    public class HelloWorldController : ApiController
     {
         // Inside your controller, define the actions you want to support.
         // By convention, actions matching HTTP method names will be
         // matched the requests to that method. If you don't follow this
         // convention, you can use the AcceptVerbsAttribute or any of
-        // the shortcut versions, such as HttpGet, HttpPost, etc.
+        // the instances such as HttpGet, HttpPost, etc.
         // Here, we will just define a Get method.
         public string Get()
         {
             return Helpers.__;
         }
+    }
 
-        // We'll amend our existing controller with a Post method
-        // that will return whatever is POSTed. 
+    public static partial class AboutControllers
+    {
+        public static void CreateAnEchoController()
+        {
+            using (var config = new HttpConfiguration())
+            using (var server = new HttpServer(config))
+            using (var client = new HttpClient(server))
+            {
+                config.Routes.MapHttpRoute("Api", "api/{controller}");
+
+                // Now send a POST request from the client to retrieve the result.
+                var uri = "http://example.org/api/echo";
+                var content = new StringContent("Hello, ApiController!");
+                using (var response = client.PostAsync(uri, content).Result)
+                {
+                    var body = response.Content.ReadAsStringAsync().Result;
+                    Helpers.AssertEquality("\"Hello, ApiController!\"", body);
+                    Helpers.AssertEquality(Helpers.__, response.Content.Headers.ContentType.MediaType);
+                }
+            }
+        }
+    }
+
+    // We'll amend our existing controller with a Post method
+    // that will return whatever is POSTed. 
+    public class EchoController : ApiController
+    {
         public string Post()
         {
             return Helpers.__;
         }
     }
 
-    class TestFixedController : ApiController
+    public static partial class AboutControllers
+    {
+        public static void AreYouSureYouMadeAnEchoController()
+        {
+            using (var config = new HttpConfiguration())
+            using (var server = new HttpServer(config))
+            using (var client = new HttpClient(server))
+            {
+                config.Routes.MapHttpRoute("Api", "api/{controller}");
+
+                var uri = "http://example.org/api/testfixed";
+                var content = new StringContent("Hello, ApiController!");
+                using (var response = client.PostAsync(uri, content).Result)
+                {
+                    var body = response.Content.ReadAsStringAsync().Result;
+                    Helpers.AssertEquality(Helpers.__, body);
+                    // Note that this passes for a test of the request's content type as we copied the value above.
+                    Helpers.AssertEquality(Helpers.__, response.Content.Headers.ContentType.MediaType);
+                }
+            }
+        }
+    }
+
+    public class TestFixedController : ApiController
     {
         // Contrary to what you might have expected, copying a request body is not quite as simple
         // as it may at first appear. The previous step can be successfully completed by returning
@@ -52,7 +135,10 @@ namespace Koans
         // response completes. You have to copy the content to a new content.
         public async Task<HttpResponseMessage> Post(HttpRequestMessage request)
         {
-            Console.WriteLine("Received {0}", request);
+            // NOTE that here we model bind the request.
+            // The ApiController already has a Request property,
+            // so you don't need to do this.
+
             var stream = new MemoryStream();
             await request.Content.CopyToAsync(stream);
 
@@ -64,63 +150,6 @@ namespace Koans
                 content.Headers.TryAddWithoutValidation(header.Key, header.Value);
 
             return new HttpResponseMessage(HttpStatusCode.OK) { Content = content };
-        }
-    }
-
-    [Koan(Sort = 2)]
-    public static class AboutControllers
-    {
-        [Koan]
-        public static void SimpleHelloWorldController()
-        {
-            // Controllers can't be found without routing. Here, we use a very generic
-            // uri template and map it using the `MapHttpRoute` extension method.
-            // If you have worked with MVC, this will be very familiar. In a larger
-            // project, this would most likely be defined in your Global.asax file.
-            Core.Config.Routes.MapHttpRoute("Api", "api/{controller}");
-
-            // Now send a GET request from the client to retrieve the result.
-            using (var request = new HttpRequestMessage(HttpMethod.Get, "http://example.org/api/test"))
-            using (var response = Core.Client.SendAsync(request, Core.Cts.Token).Result)
-            {
-                var body = response.Content.ReadAsStringAsync().Result;
-                Helpers.AssertEquality("\"Hello, ApiController!\"", body);
-            }
-
-            Core.Reset();
-        }
-
-        public static void CreateAnEchoController()
-        {
-            Core.Config.Routes.MapHttpRoute("Api", "api/{controller}");
-
-            // Now send a POST request from the client to retrieve the result.
-            using (var request = new HttpRequestMessage(HttpMethod.Post, "http://example.org/api/test") { Content = new StringContent("Hello, ApiController!") })
-            using (var response = Core.Client.SendAsync(request, Core.Cts.Token).Result)
-            {
-                var body = response.Content.ReadAsStringAsync().Result;
-                Helpers.AssertEquality("\"Hello, ApiController!\"", body);
-                Helpers.AssertEquality(Helpers.__, response.Content.Headers.ContentType.MediaType);
-            }
-
-            Core.Reset();
-        }
-
-        public static void AreYouSureYouMadeAnEchoController()
-        {
-            Core.Config.Routes.MapHttpRoute("Api", "api/{controller}");
-
-            // Now send a POST request from the client to retrieve the result.
-            using (var request = new HttpRequestMessage(HttpMethod.Post, "http://example.org/api/testfixed") { Content = new StringContent("Hello, ApiController!") })
-            using (var response = Core.Client.SendAsync(request, Core.Cts.Token).Result)
-            {
-                var body = response.Content.ReadAsStringAsync().Result;
-                Helpers.AssertEquality(Helpers.__, body);
-                // Note that this passes for a test of the request's content type as we copied the value above.
-                Helpers.AssertEquality(Helpers.__, response.Content.Headers.ContentType.MediaType);
-            }
-
-            Core.Reset();
         }
     }
 }
