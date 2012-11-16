f0a27df4668cbdc677a90189641c6e4d5536dbd2
Ryan Riley <ryan.riley@panesofglass.org>
2012-11-16 14:24:43 -0600

    Begin simplifying the koans.

--- Koans/AboutBasics.cs
+++ Koans/AboutBasics.cs
@@ -21,70 +21,43 @@ namespace Koans
         [Koan]
         public static void NotFound()
         {
-            // Create a configuration. Web API uses a code-based configuration
-            // rather than the XML-based configuration familiar to WCF programmers.
-            using (var config = new HttpConfiguration())
-            // Create an HttpServer. HttpServer is the core for all other server
-            // implementations. We'll discuss this in more detail later.
-            using (var server = new HttpServer(config))
-            // Create an HttpClient. HttpClient can be used without the server
-            // to hit the public web, but passing in a server will allow you
-            // direct access.
-            using (var client = new HttpClient(server))
             // Send a GET message. The Web API API is asynchronous.
             // You should avoid calling .Result, but for testing,
             // this is desired. Fortunately, C# 5 helps deal with async.
-            using (var response = client.GetAsync("http://go.com/").Result)
-            {
-                // We will use the Microsoft.AspNet.WebApi.Tracing package to print
-                // trace information from Web API. Check your output window so see
-                // how Web API works.
-                TraceConfig.Register(config);
-
+            using (var response = WebApiKoans.Client.GetAsync("http://go.com/").Result)
                 Helpers.AssertEquality(Helpers.__, response.StatusCode);
-            }
         }
 
         [Koan]
         public static void FirstRoute()
         {
-            using (var config = new HttpConfiguration())
-            using (var server = new HttpServer(config))
-            using (var client = new HttpClient(server))
-            {
-                TraceConfig.Register(config);
+            // In order to get anything other than a 404 response,
+            // you first must add a route on the configuration.
+            // Note how similar routing is to routing in MVC.
+            // We'll explore routing further a little later.
+            GlobalConfiguration.Configuration.Routes.MapHttpRoute(
+                name: "Default",
+                routeTemplate: "api"
+            );
 
-                // In order to get anything other than a 404 response,
-                // you first must add a route on the configuration.
-                // Note how similar routing is to routing in MVC.
-                // We'll explore routing further a little later.
-                config.Routes.MapHttpRoute("Default", "api");
-
-                using (var response = client.GetAsync("http://go.com/api").Result)
-                    Helpers.AssertEquality(Helpers.__, response.StatusCode);
-            }
+            using (var response = WebApiKoans.Client.GetAsync("http://go.com/api").Result)
+                Helpers.AssertEquality(Helpers.__, response.StatusCode);
         }
 
         [Koan]
         public static void Ok()
         {
-            using (var config = new HttpConfiguration())
-            using (var server = new HttpServer(config))
-            using (var client = new HttpClient(server))
-            {
-                TraceConfig.Register(config);
-                config.Routes.MapHttpRoute("Default", "api");
+            GlobalConfiguration.Configuration.Routes.MapHttpRoute("Default", "api");
 
-                // A route alone is insufficient to get past the 404.
-                // You also need some form of handler. ASP.NET has IHttpHandler
-                // and IHttpModule, but Web API uses ApiControllers and
-                // HttpMessageHandlers. We will cover each in detail later,
-                // but for now we will add an HttpMessageHandler for simplicity.
-                config.MessageHandlers.Add(new OkHandler());
+            // A route alone is insufficient to get past the 404.
+            // You also need some form of handler. ASP.NET has IHttpHandler
+            // and IHttpModule, but Web API uses ApiControllers and
+            // HttpMessageHandlers. We will cover each in detail later,
+            // but for now we will add an HttpMessageHandler for simplicity.
+            GlobalConfiguration.Configuration.MessageHandlers.Add(new OkHandler());
 
-                using (var response = client.GetAsync("http://go.com/api").Result)
-                    Helpers.AssertEquality(Helpers.__, response.StatusCode);
-            }
+            using (var response = WebApiKoans.Client.GetAsync("http://go.com/api").Result)
+                Helpers.AssertEquality(Helpers.__, response.StatusCode);
         }
 
         class OkHandler : DelegatingHandler
@@ -110,24 +83,18 @@ namespace Koans
         [Koan]
         public static void HelloWorld()
         {
-            using (var config = new HttpConfiguration())
-            using (var server = new HttpServer(config))
-            using (var client = new HttpClient(server))
+            GlobalConfiguration.Configuration.Routes.MapHttpRoute("Default", "api");
+            GlobalConfiguration.Configuration.MessageHandlers.Add(new HelloHandler());
+
+            using (var response = WebApiKoans.Client.GetAsync("http://go.com/api").Result)
             {
-                TraceConfig.Register(config);
-                config.Routes.MapHttpRoute("Default", "api");
-                config.MessageHandlers.Add(new HelloHandler());
+                // In addition to getting the response message, you need
+                // to also retrieve the content. Each message object has a
+                // Content property and methods to read the contents.
+                // We will look more into these in AboutContent.
+                var body = response.Content.ReadAsStringAsync().Result;
 
-                using (var response = client.GetAsync("http://go.com/api").Result)
-                {
-                    // In addition to getting the response message, you need
-                    // to also retrieve the content. Each message object has a
-                    // Content property and methods to read the contents.
-                    // We will look more into these in AboutContent.
-                    var body = response.Content.ReadAsStringAsync().Result;
-
-                    Helpers.AssertEquality("Hello, world!", body);
-                }
+                Helpers.AssertEquality("Hello, world!", body);
             }
         }
 
---
+++ Koans/GlobalConfiguration.cs
@@ -0,0 +1,57 @@
+﻿using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+
+namespace Koans
+{
+    public static class GlobalConfiguration
+    {
+        public static readonly HttpConfiguration Configuration;
+
+        static GlobalConfiguration()
+        {
+            Configuration = new HttpConfiguration();
+
+            // We will use the Microsoft.AspNet.WebApi.Tracing package to print
+            // trace information from Web API. Check your output window so see
+            // how Web API works.
+            TraceConfig.Register(Configuration);
+        }
+
+        public static void Reset()
+        {
+            JsonMediaTypeFormatter jsonFormatter = Configuration.Formatters.JsonFormatter;
+            XmlMediaTypeFormatter xmlFormatter = Configuration.Formatters.XmlFormatter;
+            Configuration.Formatters.Clear();
+            Configuration.Formatters.Add(jsonFormatter);
+            Configuration.Formatters.Add(xmlFormatter);
+            Configuration.MessageHandlers.Clear();
+            Configuration.Routes.Clear();
+        }
+
+        public static void Dispose()
+        {
+            if (Configuration != null)
+                Configuration.Dispose();
+        }
+    }
+
+    public static class WebApiKoans
+    {
+        public static HttpServer Server = new HttpServer(GlobalConfiguration.Configuration);
+        public static HttpClient Client = new HttpClient(Server, true);
+
+        public static void Dispose()
+        {
+            try
+            {
+                if (Client != null)
+                {
+                    Client.Dispose();
+                    Client = null;
+                }
+            }
+            catch {}
+        }
+    }
+}
